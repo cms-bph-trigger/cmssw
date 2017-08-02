872b99c267bce4cb3915820203ad88caa286557b
Alessio Boletti <boletti@pd.infn.it>
2017-08-02 12:15:09 +0200

    Fixes in BPHMonitor configuration files

--- DQMOffline/Trigger/python/BPHMonitor_cfi.py
+++ DQMOffline/Trigger/python/BPHMonitor_cfi.py
@@ -91,8 +91,8 @@ hltBPHmonitoring.numGenericTriggerEventPSet.andOrL1      = cms.bool(True)# True:
 hltBPHmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "hltTriggerSummaryAOD","","HLT")
 #hltBPHmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "hltTriggerSummaryRAW")
 hltBPHmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_Dimuon0_Jpsi_L1_NoOS_v*") # HLT_ZeroBias_v*
-hltBPHmonitoring.numGenericTriggerEventPSet.l1Algorithms      = cms.vstring("L1_DoubleMu0_SQ") # HLT_ZeroBias_v*
-hltBPHmonitoring.numGenericTriggerEventPSet.hltDBKey      = cms.string("diMu10")
+# hltBPHmonitoring.numGenericTriggerEventPSet.l1Algorithms      = cms.vstring("L1_DoubleMu0_SQ") # HLT_ZeroBias_v*
+# hltBPHmonitoring.numGenericTriggerEventPSet.hltDBKey      = cms.string("diMu10")
 hltBPHmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
 hltBPHmonitoring.numGenericTriggerEventPSet.errorReplyL1 = cms.bool( True )
 hltBPHmonitoring.numGenericTriggerEventPSet.l1BeforeMask = cms.bool( True )
@@ -103,9 +103,13 @@ hltBPHmonitoring.denGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True
 hltBPHmonitoring.denGenericTriggerEventPSet.dcsInputTag   = cms.InputTag( "scalersRawToDigi" )
 #hltBPHmonitoring.denGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
 hltBPHmonitoring.denGenericTriggerEventPSet.hltPaths  = cms.vstring( "HLT_Mu7p5_Track2_Jpsi_v*" )#reference
-hltBPHmonitoring.denGenericTriggerEventPSet.l1Algorithms      = cms.vstring("L1_DoubleMu0_SQ") # HLT_ZeroBias_v*
+# hltBPHmonitoring.denGenericTriggerEventPSet.l1Algorithms      = cms.vstring("L1_DoubleMu0_SQ") # HLT_ZeroBias_v*
 hltBPHmonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32 ( 0,1,2,3,5,6,7,8,9,12,13,14,15,16,17,20,22,24, 25, 26, 27, 28, 29 ) # 24-27: strip, 28-29: pixel
 hltBPHmonitoring.denGenericTriggerEventPSet.andOrDcs      = cms.bool( False )
 hltBPHmonitoring.denGenericTriggerEventPSet.errorReplyDcs = cms.bool( True )
 hltBPHmonitoring.denGenericTriggerEventPSet.verbosityLevel = cms.uint32(0)
+
+# hltBPHmonitoring.denGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "hltTriggerSummaryAOD","","HLT" )
+hltBPHmonitoring.denGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
+hltBPHmonitoring.denGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
 
